ab0c10a20f0c096416669ec9af47ed4277a6d081
Smokeybro916 <kyleluangasa@gmail.com>
2024-06-17 18:10:58 -0400

    finished flash for success and error

--- app.js
+++ app.js
@@ -48,8 +48,14 @@ const sessionConfig= {
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }
-app.use(session(sessionConfig)
+app.use(session(sessionConfig))
 app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
